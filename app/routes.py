95b572ff502ca37a7f1bf31d55f8b251d4ca5e07
TheAllenAnker <theallenanker@gmail.com>
2018-05-22 20:28:47 +0800

    Using url_for() to get the URL instead of hardcoded.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 #Author: Allen Anker
 #Created by Fenyr_Allen on 22/05/2018
 
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -28,5 +28,5 @@ def login():
     if form.validate_on_submit():
         flash('Login request for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
