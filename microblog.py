e7f8e4c27b693bbeea066e5d289be5e2865e00b5
TheAllenAnker <theallenanker@gmail.com>
2018-05-24 08:52:32 +0800

    Adding register, login and logout support.

--- microblog.py
+++ microblog.py
@@ -1,5 +1,10 @@
-#Author: Allen Anker
-#Created by Fenyr_Allen on 22/05/2018
+# Author: Allen Anker
+# Created by Fenyr_Allen on 22/05/2018
 
-from app import app
+from app import app, db
+from app.models import User, Post
 
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
